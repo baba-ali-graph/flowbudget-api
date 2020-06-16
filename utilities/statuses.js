2a858bffb50e8acdb7c015ab4d8256d8c089f493
Baba Ali AbdulAziz <48876447+AleeyCreative@users.noreply.github.com>
2020-06-16 10:55:22 +0100

    Add some server status code

--- utilities/statuses.js
+++ utilities/statuses.js
@@ -1,6 +1,10 @@
 // temporary fixes, might not neccsarily be the appropriate status code.
 module.exports = {
-    AUTH_STATUS_FAIL:204,
-    MAIL_STATUS_FAIL:205,
-    SUCCESS:200
+    AUTH_STATUS_FAIL:401,
+    MAIL_STATUS_FAIL:"?",
+    SUCCESS:200,
+    DATABASE_FAIL:501,
+    REQUEST_ERR:400,
+    SUCCESS_NO_RESP:204,
+    SUCCESS_DONE:205
 }
